948c4eeeafbbd38b16352420bf998471c856ddb8
Anas <muhammadanasbinadam@gmail.com>
2024-03-23 10:22:10 +0800

    Fix avatar image

--- src/app/dashboard/components/preview.tsx
+++ src/app/dashboard/components/preview.tsx
@@ -1,4 +1,5 @@
 import { UserDetails } from "@/interface/user-details";
+import supabase from "@/utils/supabase";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
@@ -11,20 +12,42 @@ interface Links {
 
 export default function Preview(data: UserDetails) {
   const [links, setLinks] = useState([]);
+  const [avatar, setAvatar] = useState<string>();
 
   useEffect(() => {
-    if (data && data.links) {
-      const links = data.links || '';
-      setLinks(JSON.parse(links));
-      console.log(JSON.parse(links));
+    if (data) {
+      if (data.avatar) {
+        downloadAvatar(data.avatar!);
+      }
+
+      if (data.links) {
+        const links = data.links || '';
+        setLinks(JSON.parse(links));
+        console.log(JSON.parse(links));
+      }
     }
   },[])
+
+  const downloadAvatar = async (avatarPath: string) => {
+    const { data, error } = await supabase
+      .storage
+      .from('link-bucket')
+      .download(avatarPath)
+
+    if (data) {
+      const objectUrl = URL.createObjectURL(data);
+      setAvatar(objectUrl);
+    }
+    if (error) {
+      console.log(error);
+    }
+  }
 
   return (
     <div className="bg-white w-full md:max-w-[400px] h-screen py-10 px-4 border mx-auto">
       <img 
         className="w-16 h-16 mx-auto bg-gray-50 rounded-full flex justify-center items-center overflow-hidden"
-        src={data.profileImage ? data.profileImage:'https://placehold.co/50'} 
+        src={avatar ? avatar:'https://placehold.co/50'} 
         alt="uploaded-image"
       />
       <h6 className="text-sm md:text-normal text-center mt-2">@{data.username}</h6>
