b8103d33b7dcaa6cd109819ca0a081c4abb9e1cf
Anas <muhammadanasbinadam@gmail.com>
2024-03-24 22:32:08 +0800

    Fix redux hooks

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -17,26 +17,8 @@ export const store = createStore(persistedReducer);
 
 export const persistor = persistStore(store);
 
-// export const store = configureStore({
-//   reducer: {
-//     auth: persistedReducer,
-//     link: linkReducer
-//   },
-//     middleware: (getDefaultMiddleware) =>
-//       getDefaultMiddleware({ serializableCheck: false })
-// })
-
-// Infer the type of makeStore
-// export type AppStore = ReturnType<typeof store>
-// Infer the `RootState` and `AppDispatch` types from the store itself
-// export type RootState = ReturnType<AppStore['getState']>
-// export type AppDispatch = AppStore['dispatch']
-
-// export const persistor = persistStore(store)
 
 export type AppStore = typeof store;
-// export type RootState = ReturnTypestore.getState;
-// export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<AppStore['getState']>
 export type AppDispatch = AppStore['dispatch']
 
