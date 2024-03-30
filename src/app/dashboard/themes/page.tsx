80ae63cb5a32531a0b2b1ae4c303be212166931c
Anas <muhammadanasbinadam@gmail.com>
2024-03-30 21:28:13 +0800

    Added premium themes

--- src/app/dashboard/themes/page.tsx
+++ src/app/dashboard/themes/page.tsx
@@ -8,20 +8,37 @@ import { ITheme, themes } from "./theme-list";
 import { setTheme } from "@/lib/store/linkSlice";
 import Image from "next/image";
 import supabase from "@/utils/supabase";
+import { LockKeyhole, Check } from "lucide-react";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog"
+import { Button } from "@/components/ui/button";
+import { DialogDescription } from "@radix-ui/react-dialog";
 
 export default function Themes() {
   const [loading, setLoading] = useState<boolean>(false);
+  const [open, setOpen] = useState<boolean>(false);
   const linkDetails: any = useAppSelector(state => state.link.linkDetails);
   const selectedTheme = useAppSelector(state => state.link.selectedTheme);
   const dispatch = useAppDispatch();
 
   const selectTheme = async (theme: ITheme) => {
-    dispatch(setTheme(theme));
+    const userTier = "Free";
 
-    const { error } = await supabase
-      .from('appearance')
-      .update({ theme_class: theme.themeClass })
-      .eq('uid', linkDetails.id)
+    if (userTier === "Free" && theme.type === "Free") {
+      dispatch(setTheme(theme));
+  
+      const { error } = await supabase
+        .from('appearance')
+        .update({ theme_class: theme.themeClass })
+        .eq('uid', linkDetails.id)
+    } else {
+      setOpen(true);
+    }
   }
 
   return (
@@ -42,7 +59,17 @@ export default function Themes() {
                       <div className={`border rounded overflow-hidden cursor-pointer ${selectedTheme?.name === theme.name ? 'border-black border-dashed border-2':''}`}>
                         <Image src={theme.previewImage} alt={theme.name} width={120} height={200} className="object-cover" />
                       </div>
-                      <h6 className="text-sm text-center mt-1">{theme.name}</h6>
+                      <h6 className="text-sm text-center mt-1">
+                        {theme.name}
+                      </h6>
+                      {theme.type === 'Premium' && 
+                        <div>
+                          <span className="absolute top-2 right-4 text-xs text-white bg-black rounded py-1 px-2 flex flex-row items-center">
+                            Upgrade
+                            <LockKeyhole className="w-3.5 h-3.5 ml-1 text-white" />
+                          </span>
+                        </div>
+                      }
                     </div>
                   )
                 })}
@@ -62,6 +89,48 @@ export default function Themes() {
           </div>
         </div>
       </div>
+
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogContent className="sm:max-w-md">
+          <DialogHeader>
+            <DialogTitle>Try Premium to use this feature</DialogTitle>
+            <DialogDescription className="text-sm text-gray-500">
+              Get 30 days of Pro for free. Cancel anytime.
+            </DialogDescription>
+          </DialogHeader>
+          <div className="flex flex-col my-2 text-sm">
+            <h5>
+              Grow, know and own your audience with a free 30-day Pro trial. Just RM16.50 MYR/month after. Get instant access to Pro features, including:
+            </h5>
+            <div className="flex flex-col gap-2 mt-4">
+              <div className="flex flex-row">
+                <Check className="w-5 h-5 mt-0.5 mr-2" />
+                Lifetime analytics and powerful insights
+              </div>
+              <div className="flex flex-row">
+                <Check className="w-5 h-5 mt-0.5 mr-2" />
+                Unlimited customization for your brand
+              </div>
+              <div className="flex flex-row">
+                <Check className="w-5 h-5 mt-0.5 mr-2" />
+                Integration with third-party marketing tools
+              </div>
+              <div className="flex flex-row">
+                <Check className="w-5 h-5 mt-0.5 mr-2" />
+                Priority customer support and much more
+              </div>
+            </div>
+          </div>
+          <DialogFooter className="justify-end">
+            <Button type="button" variant="outline">
+              View all plans
+            </Button>
+            <Button type="button">
+              Free for 30 days
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
     </div>
   )
 }
--- src/app/dashboard/themes/theme-list.ts
+++ src/app/dashboard/themes/theme-list.ts
@@ -1,48 +1,87 @@
 export interface ITheme {
   name?: string,
   themeClass?: string,
-  previewImage?: string
+  previewImage?: string,
+  type?: string
 }
 
 export const themes = [
   {
     name: 'Iris Snow',
     themeClass: 'iris-snow',
-    previewImage: '/themes/iris-snow.png'
+    previewImage: '/themes/iris-snow.png',
+    type: 'Free'
   },
   {
     name: 'Iris White',
     themeClass: 'iris-white',
-    previewImage: '/themes/iris-white.png'
+    previewImage: '/themes/iris-white.png',
+    type: 'Free'
   },
   {
     name: 'Iris Grey',
     themeClass: 'iris-grey',
-    previewImage: '/themes/iris-grey.png'
+    previewImage: '/themes/iris-grey.png',
+    type: 'Free'
   },
   {
     name: 'Iris Blue',
     themeClass: 'iris-blue',
-    previewImage: '/themes/iris-blue.png'
+    previewImage: '/themes/iris-blue.png',
+    type: 'Free'
   },
   {
     name: 'Iris Purple',
     themeClass: 'iris-purple',
-    previewImage: '/themes/iris-purple.png'
+    previewImage: '/themes/iris-purple.png',
+    type: 'Free'
   },
   {
     name: 'Iris Forest',
     themeClass: 'iris-forest',
-    previewImage: '/themes/iris-forest.png'
+    previewImage: '/themes/iris-forest.png',
+    type: 'Free'
   },
   {
     name: 'Iris Dark',
     themeClass: 'iris-dark',
-    previewImage: '/themes/iris-dark.png'
+    previewImage: '/themes/iris-dark.png',
+    type: 'Free'
   },
   {
     name: 'Iris Black',
     themeClass: 'iris-black',
-    previewImage: '/themes/iris-black.png'
-  }
+    previewImage: '/themes/iris-black.png',
+    type: 'Free'
+  },
+  {
+    name: 'Rainbow',
+    themeClass: 'rainbow',
+    previewImage: '/themes/rainbow.png',
+    type: 'Premium'
+  },
+  {
+    name: 'Flurry Blue',
+    themeClass: 'flurry-blue',
+    previewImage: '/themes/flurry-blue.png',
+    type: 'Premium'
+  },
+  {
+    name: 'Flurry Green',
+    themeClass: 'flurry-green',
+    previewImage: '/themes/flurry-green.png',
+    type: 'Premium'
+  },
+  {
+    name: 'Flurry Pink',
+    themeClass: 'flurry-pink',
+    previewImage: '/themes/flurry-pink.png',
+    type: 'Premium'
+  },
+  {
+    name: 'Windows XP',
+    themeClass: 'windows-xp',
+    previewImage: '/themes/windows-xp.png',
+    type: 'Premium'
+  },
 ]
